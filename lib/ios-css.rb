c8b0ecef440ff80be389c372eab7f97a746f11a9
Philipp Bosch <hello@pb.io>
2011-06-09 17:38:13 +0200

    Navigation bar improvements. Noise added.

--- lib/ios-css.rb
+++ lib/ios-css.rb
@@ -1,3 +1,5 @@
 require 'compass'
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('ios-css', :path => extension_path)
+
+require File.join(File.dirname(__FILE__), 'ios-css', 'sass_extensions')
