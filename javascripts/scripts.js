467d070d46d31fa642f828d811e69f0988950d64
Philipp Bosch <hello@pb.io>
2011-06-09 17:41:54 +0200

    New design. New examples. New new.

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,19 +1,8 @@
 (function($) {
     $(document).ready(function() {
-        $('.example button.switch').click(function() {
-            var $button = $(this),
-                $app = $button.closest('.example').find('.app');
-            
-            $app.attr('class', $button.data('original-class'));
-            $app.addClass($button.data('switch-class'));
-        }).each(function(i, el) {
-            var $button = $(el),
-                $app = $button.closest('.example').find('.app');
-            
-            $button.data('original-class', $app.attr('class'));
-            if ($button.data('switch-default') !== undefined) {
-                $button.trigger('click');
-            }
+        $('#toc a').click(function() {
+            $('#toc a.selected').removeClass('selected');
+            $(this).addClass('selected');
         });
     });
 })(jQuery);
